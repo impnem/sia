89368e3f89f3bb0f5242424d88e64f275b390cc3
impnem <impnem@gmail.com>
2022-05-28 22:05:34 +0900

    modified design of item on routine page

--- src/components/Routine/RoutineMain.vue
+++ src/components/Routine/RoutineMain.vue
@@ -32,20 +32,27 @@
           :key="c"
           flat
           color="white--dark"
-          class="my-1"
+          class="mx-1 my-1 rounded-lg"
         >
-          <v-card-text
-            class="black--text"
+          <v-alert
+            class="px-0 py-0 mx-0 my-0"
+            border="left"
+            colored-border
+            :color="`${ content.colorTag }`"
           >
-            <v-checkbox
-              v-model="content.check"
-              class="my-0 py-0"
-              :label="`${ content.title }`"
-              text-color="primary"
-              hide-details
+            <v-card-text
+              class="black--text"
             >
-            </v-checkbox>
-          </v-card-text>
+              <v-checkbox
+                v-model="content.check"
+                class="my-0 py-0"
+                :label="`${ content.title }`"
+                text-color="primary"
+                hide-details
+              >
+              </v-checkbox>
+            </v-card-text>
+          </v-alert>
         </v-card>
       </v-tab-item>
     </v-tabs-items>
@@ -62,6 +69,7 @@
 </template>
 
 <script>
+
 export default {
   data () {
     return {
@@ -73,47 +81,56 @@ export default {
             {
               time: '0700',
               title: '7시 기상',
-              check: true
+              check: true,
+              colorTag: 'red darken-2'
             },
             {
               time: '0800',
               title: '8시 아침식사',
-              check: true
+              check: true,
+              colorTag: 'orange darken-2'
             },
             {
               time: '0900',
               title: '9시 출근',
-              check: true
+              check: true,
+              colorTag: 'yellow darken-2'
             },
             {
               time: '1200',
               title: '12시 점심식사',
-              check: true
+              check: true,
+              colorTag: 'green darken-2'
             },
             {
               time: '1800',
               title: '18시 퇴근',
-              check: false
+              check: false,
+              colorTag: 'blue darken-2'
             },
             {
               time: '1900',
               title: '19시 저녁식사',
-              check: false
+              check: false,
+              colorTag: 'indigo darken-2'
             },
             {
               time: '2000',
               title: '20시 자기계발',
-              check: false
+              check: false,
+              colorTag: 'deep-purple darken-2'
             },
             {
               time: '2200',
               title: '22시 취침준비',
-              check: false
+              check: false,
+              colorTag: 'red darken-2'
             },
             {
               time: '2300',
               title: '23시 취침',
-              check: false
+              check: false,
+              colorTag: 'orange darken-2'
             }
           ]
         },
@@ -123,47 +140,56 @@ export default {
             {
               time: '0700',
               title: '7시 기상',
-              check: true
+              check: true,
+              colorTag: 'red darken-2'
             },
             {
               time: '0800',
               title: '8시 아침식사',
-              check: true
+              check: true,
+              colorTag: 'orange darken-2'
             },
             {
               time: '0900',
               title: '9시 출근',
-              check: true
+              check: true,
+              colorTag: 'yellow darken-2'
             },
             {
               time: '1200',
               title: '12시 점심식사',
-              check: true
+              check: true,
+              colorTag: 'green darken-2'
             },
             {
               time: '1800',
               title: '18시 퇴근',
-              check: false
+              check: false,
+              colorTag: 'blue darken-2'
             },
             {
               time: '1900',
               title: '19시 저녁식사',
-              check: false
+              check: false,
+              colorTag: 'indigo darken-2'
             },
             {
               time: '2000',
               title: '20시 자기계발',
-              check: false
+              check: false,
+              colorTag: 'deep-purple darken-2'
             },
             {
               time: '2200',
               title: '22시 취침준비',
-              check: false
+              check: false,
+              colorTag: 'red darken-2'
             },
             {
               time: '2300',
               title: '23시 취침',
-              check: false
+              check: false,
+              colorTag: 'orange darken-2'
             }
           ]
         },
@@ -173,47 +199,56 @@ export default {
             {
               time: '0700',
               title: '7시 기상',
-              check: true
+              check: true,
+              colorTag: 'red darken-2'
             },
             {
               time: '0800',
               title: '8시 아침식사',
-              check: true
+              check: true,
+              colorTag: 'orange darken-2'
             },
             {
               time: '0900',
               title: '9시 출근',
-              check: true
+              check: true,
+              colorTag: 'yellow darken-2'
             },
             {
               time: '1200',
               title: '12시 점심식사',
-              check: true
+              check: true,
+              colorTag: 'green darken-2'
             },
             {
               time: '1800',
               title: '18시 퇴근',
-              check: false
+              check: false,
+              colorTag: 'blue darken-2'
             },
             {
               time: '1900',
               title: '19시 저녁식사',
-              check: false
+              check: false,
+              colorTag: 'indigo darken-2'
             },
             {
               time: '2000',
               title: '20시 자기계발',
-              check: false
+              check: false,
+              colorTag: 'deep-purple darken-2'
             },
             {
               time: '2200',
               title: '22시 취침준비',
-              check: false
+              check: false,
+              colorTag: 'red darken-2'
             },
             {
               time: '2300',
               title: '23시 취침',
-              check: false
+              check: false,
+              colorTag: 'orange darken-2'
             }
           ]
         },
@@ -223,47 +258,56 @@ export default {
             {
               time: '0700',
               title: '7시 기상',
-              check: true
+              check: true,
+              colorTag: 'red darken-2'
             },
             {
               time: '0800',
               title: '8시 아침식사',
-              check: true
+              check: true,
+              colorTag: 'orange darken-2'
             },
             {
               time: '0900',
               title: '9시 출근',
-              check: true
+              check: true,
+              colorTag: 'yellow darken-2'
             },
             {
               time: '1200',
               title: '12시 점심식사',
-              check: true
+              check: true,
+              colorTag: 'green darken-2'
             },
             {
               time: '1800',
               title: '18시 퇴근',
-              check: false
+              check: false,
+              colorTag: 'blue darken-2'
             },
             {
               time: '1900',
               title: '19시 저녁식사',
-              check: false
+              check: false,
+              colorTag: 'indigo darken-2'
             },
             {
               time: '2000',
               title: '20시 자기계발',
-              check: false
+              check: false,
+              colorTag: 'deep-purple darken-2'
             },
             {
               time: '2200',
               title: '22시 취침준비',
-              check: false
+              check: false,
+              colorTag: 'red darken-2'
             },
             {
               time: '2300',
               title: '23시 취침',
-              check: false
+              check: false,
+              colorTag: 'orange darken-2'
             }
           ]
         },
@@ -273,47 +317,56 @@ export default {
             {
               time: '0700',
               title: '7시 기상',
-              check: true
+              check: true,
+              colorTag: 'red darken-2'
             },
             {
               time: '0800',
               title: '8시 아침식사',
-              check: true
+              check: true,
+              colorTag: 'orange darken-2'
             },
             {
               time: '0900',
               title: '9시 출근',
-              check: true
+              check: true,
+              colorTag: 'yellow darken-2'
             },
             {
               time: '1200',
               title: '12시 점심식사',
-              check: true
+              check: true,
+              colorTag: 'green darken-2'
             },
             {
               time: '1800',
               title: '18시 퇴근',
-              check: false
+              check: false,
+              colorTag: 'blue darken-2'
             },
             {
               time: '1900',
               title: '19시 저녁식사',
-              check: false
+              check: false,
+              colorTag: 'indigo darken-2'
             },
             {
               time: '2000',
               title: '20시 자기계발',
-              check: false
+              check: false,
+              colorTag: 'deep-purple darken-2'
             },
             {
               time: '2200',
               title: '22시 취침준비',
-              check: false
+              check: false,
+              colorTag: 'red darken-2'
             },
             {
               time: '2300',
               title: '23시 취침',
-              check: false
+              check: false,
+              colorTag: 'orange darken-2'
             }
           ]
         },
@@ -323,62 +376,74 @@ export default {
             {
               time: '0800',
               title: '8시 기상',
-              check: true
+              check: true,
+              colorTag: 'red darken-2'
             },
             {
               time: '0900',
               title: '9시 아침식사',
-              check: true
+              check: true,
+              colorTag: 'orange darken-2'
             },
             {
               time: '1000',
               title: '10시 빨래',
-              check: true
+              check: true,
+              colorTag: 'yellow darken-2'
             },
             {
               time: '1100',
               title: '11시 독서',
-              check: true
+              check: true,
+              colorTag: 'green darken-2'
             },
             {
               time: '1200',
               title: '12시 점심식사',
-              check: false
+              check: false,
+              colorTag: 'blue darken-2'
             },
             {
               time: '1300',
               title: '13시 블로그 글 올리기',
-              check: false
+              check: false,
+              colorTag: 'indigo darken-2'
             },
             {
               time: '1600',
               title: '16시 자기계발',
-              check: false
+              check: false,
+              colorTag: 'deep-purple darken-2'
             },
             {
               time: '1800',
               title: '18시 저녁식사',
-              check: false
+              check: false,
+              colorTag: 'red darken-2'
             },
             {
               time: '1900',
               title: '19시 영화보기',
-              check: false
+              check: false,
+              colorTag: 'orange darken-2'
             },
             {
               time: '2100',
               title: '21시 독서',
-              check: false
+              check: false,
+              colorTag: 'yellow darken-2'
             },
             {
               time: '2200',
               title: '22시 취침준비',
-              check: false
+              check: false,
+              colorTag: 'green darken-2'
             },
             {
               time: '2300',
               title: '23시 취침',
-              check: false
+              check: false,
+              colorTag: 'blue darken-2'
             }
           ]
         },
@@ -388,62 +453,74 @@ export default {
             {
               time: '0800',
               title: '8시 기상',
-              check: true
+              check: true,
+              colorTag: 'red darken-2'
             },
             {
               time: '0900',
               title: '9시 아침식사',
-              check: true
+              check: true,
+              colorTag: 'orange darken-2'
             },
             {
               time: '1000',
               title: '10시 빨래',
-              check: true
+              check: true,
+              colorTag: 'yellow darken-2'
             },
             {
               time: '1100',
               title: '11시 독서',
-              check: true
+              check: true,
+              colorTag: 'green darken-2'
             },
             {
               time: '1200',
               title: '12시 점심식사',
-              check: false
+              check: false,
+              colorTag: 'blue darken-2'
             },
             {
               time: '1300',
               title: '13시 블로그 글 올리기',
-              check: false
+              check: false,
+              colorTag: 'indigo darken-2'
             },
             {
               time: '1600',
               title: '16시 자기계발',
-              check: false
+              check: false,
+              colorTag: 'deep-purple darken-2'
             },
             {
               time: '1800',
               title: '18시 저녁식사',
-              check: false
+              check: false,
+              colorTag: 'red darken-2'
             },
             {
               time: '1900',
               title: '19시 영화보기',
-              check: false
+              check: false,
+              colorTag: 'orange darken-2'
             },
             {
               time: '2100',
               title: '21시 독서',
-              check: false
+              check: false,
+              colorTag: 'yellow darken-2'
             },
             {
               time: '2200',
               title: '22시 취침준비',
-              check: false
+              check: false,
+              colorTag: 'green darken-2'
             },
             {
               time: '2300',
               title: '23시 취침',
-              check: false
+              check: false,
+              colorTag: 'blue darken-2'
             }
           ]
         }
