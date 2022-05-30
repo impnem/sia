74fcb0d8ea45b006e43ec99b98e553d27e94b458
impnem <impnem@gmail.com>
2022-05-30 22:13:49 +0900

    modified admin page

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,65 +1,13 @@
 <template>
-  <div>
-    <!-- <v-btn @click="save"><v-icon>mdi-pencil</v-icon>현재 server db 올리기</v-btn>
-    <v-btn @click="checkfs"><v-icon>mdi-pencil</v-icon>현재 fs db 보기</v-btn>
-    <v-btn @click="checkLogin"><v-icon>mdi-pencil</v-icon>현재 login 정보 보기</v-btn>
-    <v-btn @click="check"><v-icon>mdi-pencil</v-icon>사용자 첫 명언넣기</v-btn> -->
-  </div>
+  <admin-main></admin-main>
 </template>
 
 <script>
-// import { onValue } from '@firebase/database'
-import { mapState } from 'vuex'
+import AdminMain from '@/components/Admin/AdminMain.vue'
 
 export default {
-  methods: {
-    async save () {
-      try {
-        const db = this.$firebaseDB.getDatabase()
-        await this.$firebaseDB.set(this.$firebaseDB.ref(db, 'server/'), {
-          site: this.server.site,
-          users: this.server.users,
-          data: this.server.data
-        })
-      } finally { // catch 삭제함
-        this.dialog = false
-      }
-    },
-    async checkfs () {
-      const onValue = this.$firebaseDB.onValue
-      const ref = this.$firebaseDB.ref
-      const db = this.$firebaseDB.getDatabase()
-      const userId = this.login.uid
-      onValue(ref(db, 'server/users/' + userId + '/fs'), (snapshot) => {
-        console.log(snapshot.val())
-      })
-    },
-    async checkLogin () {
-      console.log(this.login)
-    },
-    check () {
-      // 첫 로그인시 명언 저장하기
-      if (this.login !== null) {
-        const onValue = this.$firebaseDB.onValue
-        const ref = this.$firebaseDB.ref
-        const db = this.$firebaseDB.getDatabase()
-        const userId = this.login.uid
-        return onValue(ref(db, 'server/users/' + userId), (snapshot) => {
-          if (snapshot.val() === null) {
-            this.$firebaseDB.set(ref(db, 'server/users/' + userId), {
-              fs: this.fs
-            })
-          }
-        })
-      }
-    }
-  },
-  computed: {
-    ...mapState({
-      server: state => state.server,
-      login: state => state.fireUser,
-      fs: state => state.local.data.fs
-    })
+  components: {
+    AdminMain
   }
 }
 </script>
